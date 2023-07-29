6e80e05e7f4a0441a759f000345ba4cf3036a73d
huge-head-dino <sjunho98@gamil.com>
2023-07-29 14:21:41 +0900

    first_version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WebcamFrame from './webcamframe';
+
+
+function App() {
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/webcamframe.js
@@ -0,0 +1,10 @@
+import React,{ usestate,useCallback, useEffect, useRef } from 'react';
+
+const WebcamFrame = () => {
+    return 
+    <div> 
+
+    </div>
+};
+
+export default WebcamFrame;
